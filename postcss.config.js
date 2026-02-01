10ef8bd0176880ddff4b986716c744015c4bff31
mohmedsalekmr-hash <mohmedsalek.mr@gmail.com>
2026-02-01 13:37:36 +0000

    Fix PostCSS config conflict and remove unused PWA plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
     plugins: {
         '@tailwindcss/postcss': {},
         autoprefixer: {},
     },
 }
-=======
-export default {
-    plugins: {
-        '@tailwindcss/postcss': {},
-        autoprefixer: {},
-    },
-}
->>>>>>> 55072ecfc7f91054f6485ed7698d8a5eb236084f
